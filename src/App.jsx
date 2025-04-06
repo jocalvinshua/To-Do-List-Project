a636304f8425b415029311414ea39ab455bfc400
jocalvin <calvinjoshua37@gmail.com>
2025-04-06 12:16:27 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
     setTasks([...tasks, task]);
   };
 
-  const toggleTaskCompleted = (id) => {
+  const completedTask = (id) => {
     setTasks(
       tasks.map((task) =>
         task.id === id ? { ...task, completed: !task.completed } : task
@@ -19,19 +19,28 @@ export default function App() {
     );
   };
 
+  const updateTask = (id) =>{
+    setTasks(
+      tasks.map((task) =>task.id === id ? { ...task, title: task.title } : task))
+  }
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
   return (
-    <div className="app">
-      <Header />
-      <Form onAddTask={addTask} />
-      <TaskList
-        tasks={tasks}
-        onToggleCheck={toggleTaskCompleted}
-        onDeleteTask={deleteTask}
-      />
+    <div className="app min-h-screen bg-gradient-to-br from-purple-100 to-blue-100 py-8 px-4">
+      <div className='max-w-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden'>
+        <div className='p-6'>
+        <Header />
+        <Form onAddTask={addTask} />
+        <TaskList
+          tasks={tasks}
+          onUpdateTask={updateTask}
+          onDeleteTask={deleteTask}
+          onCompletedTask ={completedTask}
+        />
+        </div>
+      </div>
     </div>
   );
 }
